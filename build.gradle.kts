dec6b57fd898cabd905542f033b36d98477a743f
ForteScarlet <ForteScarlet@163.com>
2024-03-25 19:11:22 +0800

    v1.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    //toolchain.languageVersion.set(JavaLanguageVersion.of(17))
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
